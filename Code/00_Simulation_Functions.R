2d0f319e0b60baac437f3e799272ca32f664fe41
Antonia Tsvetanova <64164302+antoniatsv@users.noreply.github.com>
2022-05-20 13:19:10 +0100

    Update 00_Simulation_Functions.R

--- Code/00_Simulation_Functions.R
+++ Code/00_Simulation_Functions.R
@@ -1,3 +1,23 @@
+# #######################################################################################################################
+
+# Author of code: Antonia D. Tsvetanova
+
+# This is code for a simulation study presented in a manuscript entitled: 
+# Impact of inconsistencies in missing data handlign across the pipeline 
+# of a prediction model on estimated predictive performance: A simulation study
+# Authors:
+#   Antonia Tsvetanova
+#   Matthew Sperrin
+#   Niels Peek
+#   David Jenkins
+#   Iain Buchan
+#   Stephanie Hyland
+#   Glen P. Martin
+
+# #######################################################################################################################
+
+
+
 ####-----------------------------------------------------------------------------------------
 ## Define a function to repeat the simulation across all iterations (for a given scenario)
 ####-----------------------------------------------------------------------------------------
