af2c6d95c5591aee61cbafc19c0e7b1360656b47
Antonia Tsvetanova <64164302+antoniatsv@users.noreply.github.com>
2022-06-30 10:28:50 +0100

    Update 01_Simulation_Run.R

--- Code/01_Simulation_Run.R
+++ Code/01_Simulation_Run.R
@@ -25,48 +25,58 @@
 
 #load the simulation functions
 
-source(here::here("00_Simulation_functions.R"))
+
+source("./00_Simulation_Functions.R") #for CSF
+
+#source(here::here("00_Simulation_Functions.R")) #for local run
+
+#install.packages("tidyverse")
 
 library(tidyverse)
+library(furrr)
 
-#Define a dataset that includes all combinations of simulation parameters (i.e. simulation cases) 
 
 sims_parameters <- crossing(
-  n_iter = 2, 
+  n_iter = 200, 
   N = 500000,
   N_dev = 100000,
   N_imp = 300000, 
   N_val = 100000, 
   Y_prev = c(0.1, 0.5),
   X_categorical = c(TRUE,FALSE), 
-  R_prev = c(0.05, 0.1, 0.2, 0.5),
-  beta_x1 = c(0, 0.1, 0.5, 1), 
-  beta_x2 = c(0, 0.1, 0.5, 1), 
-  beta_U = c(0, 0.1, 0.5, 1),  
+  R_prev = c(0.1, 0.2, 0.5),
+  beta_x1 = c(0, 0.5, 1), 
+  beta_x2 = c(0, 0.5, 1), 
+  beta_U = c(0, 0.5, 1),  
   gamma_x1 = c(0, 0.5, 1), 
   gamma_x2 = c(0, 0.5, 1), 
   gamma_U = c(0, 0.5, 1)
-  )
+)
 
-  
- #run the main simulation study:
 
-for(i in 1:length(sims_parameters$n_iter)){
-  
-  Results <- simulation_nrun_fnc(n_iter = sims_parameters$n_iter[i], 
-                                 N_dev = sims_parameters$N_dev[i],
-                                 N_imp = sims_parameters$N_imp[i], 
-                                 N_val = sims_parameters$N_val[i], 
-                                 Y_prev = sims_parameters$Y_prev[i],
-                                 X_categorical = sims_parameters$X_categorical[i], 
-                                 R_prev = sims_parameters$R_prev[i],
-                                 beta_x1 = sims_parameters$beta_x1[i], 
-                                 beta_x2 = sims_parameters$beta_x2[i], 
-                                 beta_U = sims_parameters$beta_U[i],  
-                                 gamma_x1 = sims_parameters$gamma_x1[i], 
-                                 gamma_x2 = sims_parameters$gamma_x2[i], 
-                                 gamma_U = sims_parameters$gamma_U[i])
-  
-  write.csv(Results, file = paste0("Results_",i,".csv"))
-  
-}
+args <- commandArgs(trailingOnly = T) #pull in all arguments from the qsub file
+s <- as.numeric(args[1]) #Note that we need to specify what class the argument is
+
+#run the main simulation study:
+
+#plan(multiprocess, workers = (availableCores()-1)) #
+###stores results alongside parameter values in a nested dataset (retaining names of outputs)
+
+simulation_results <- simulation_nrun_fnc(
+                    n_iter = sims_parameters$n_iter[s],
+                    N_dev = sims_parameters$N_dev[s],
+                    N_imp = sims_parameters$N_imp[s],
+                    N_val = sims_parameters$N_val[s],
+                    Y_prev = sims_parameters$Y_prev[s],
+                    X_categorical = sims_parameters$X_categorical[s],
+                    R_prev = sims_parameters$R_prev[s],
+                    beta_x1 = sims_parameters$beta_x1[s],
+                    beta_x2 = sims_parameters$beta_x2[s],
+                    beta_U = sims_parameters$beta_U[s],
+                    gamma_x1 = sims_parameters$gamma_x1[s],
+                    gamma_x2 = sims_parameters$gamma_x2[s],
+                    gamma_U = sims_parameters$gamma_U[s])
+
+write_rds(simulation_results, file = paste0("Results_", s,".rds"))
+
+warnings()
